669ec48f1c34e6008d4c2e163dee3ca0e537f307
Astoric <declanmcglinn@gmail.com>
2018-11-09 19:18:09 +0000

    Added info Command

--- College Discord Bot/Modules/Misc.cs
+++ College Discord Bot/Modules/Misc.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,15 @@ namespace College_Discord_Bot.Modules
     public class Misc : ModuleBase<SocketCommandContext>
     {
 
-        [Command("test")]
-        public async Task test()
+        [Command("info")]
+        public async Task info()
         {
-            await Context.Channel.SendMessageAsync("Test");
+            var embed = new EmbedBuilder();
+            embed.AddField("Language:", "C#");
+            embed.AddField("Library:", "Discord.net");
+            embed.AddField("Documentation:", "https://discord.foxbot.me/latest/api/index.html");
+            embed.AddField("Github:", "https://github.com/Astoric/COGC-Discord-Bot");
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
         }
     }
 }
